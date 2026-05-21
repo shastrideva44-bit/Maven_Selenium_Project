56bdd28014378ac32cbfc65b7eba1f1260f1ff01
shast <shast@192.168.1.12>
2026-05-21 13:48:25 +0530

    PRODUCT class Added new POM CLASS

---
+++ selenium_advance_Project/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -0,0 +1,9 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+@FindBy(xpath="//img[@title='Create Product...']")
+private WebElement createProductImgBtn;
+}
