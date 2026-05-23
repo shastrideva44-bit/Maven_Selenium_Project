a3b66119d5a6b4c6e51a22696bb28ad9c262ff6f
shast <shast@192.168.1.8>
2026-05-23 13:35:23 +0530

    manually paste ele2

--- selenium_advance_Project/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
+++ selenium_advance_Project/src/main/java/com/comcast/crm/objectrepositoryutility/Products.java
@@ -4,11 +4,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class Products {
-@FindBy(xpath="//img[@title='Create Product...']")
-private WebElement createProductImgBtn;
-//check
+    @FindBy(xpath="//img[@title='Create Product...']")
+    private WebElement createProductImgBtn;
+    //check
 
-@FindBy(name="searchBtn")
-private WebElement ele3;
+    // Manually add Sam's element back here
+    @FindBy(name="search")
+    private WebElement ele2;
+
+    // Steve's element remains here
+    @FindBy(name="searchBtn")
+    private WebElement ele3;
 }
- 
