c13b1056e8bc952a8646e53413abc75b94982875
shast <shast@192.168.1.8>
2026-05-23 15:05:52 +0530

    change baseclassname1

---
+++ selenium_advance_Project/src/test/java/com/comcast/crm/basetest/BaseClass1.java
@@ -0,0 +1,93 @@
+package com.comcast.crm.basetest;
+
+import java.sql.SQLException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import com.comcast.crm.generic.databaseutility.DataBaseUtility;
+import com.comcast.crm.generic.fileutility.ExcelUtility;
+import com.comcast.crm.generic.fileutility.FileUtility;
+import com.comcast.crm.generic.webdriverutility.JavaUtility;
+import com.comcast.crm.objectrepositoryutility.HomePage;
+import com.comcast.crm.objectrepositoryutility.LoginPage;
+
+public class BaseClass1 {
+	
+	public DataBaseUtility dbLib = new DataBaseUtility();
+	public FileUtility fLib = new FileUtility();
+	public ExcelUtility eLib = new ExcelUtility();
+	public JavaUtility jLib = new JavaUtility();
+	public WebDriver driver = null;
+	
+	public static WebDriver sdriver = null;
+	
+	@BeforeSuite(groups = {"SMOKE TEST", "REGRESSION TEST"})
+	public void configBS() throws SQLException {
+		System.out.println("===Connect to DB , Report Config===");
+		dbLib.getDbconnection();
+	}
+
+	@Parameters("BROWSER")
+	@BeforeClass(groups = {"SMOKE TEST", "REGRESSION TEST"})
+	public void configBC(String browser) throws Throwable {
+		System.out.println("==Launch the BROWSER==");
+		
+		String BROWSER = null;
+		if (browser != null) {
+			BROWSER = browser;
+		} else {
+			BROWSER = fLib.getDataFromPropertiesFile("browser");
+		}
+		
+		if(BROWSER.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if(BROWSER.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if(BROWSER.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+		
+		sdriver = driver;
+	}
+
+	@BeforeMethod(groups = {"SMOKE TEST", "REGRESSION TEST"})
+	public void configBM() throws Throwable {
+		System.out.println("=login=");
+		String URL = fLib.getDataFromPropertiesFile("url");
+		String USERNAME = fLib.getDataFromPropertiesFile("username");
+		String PASSWORD = fLib.getDataFromPropertiesFile("password");
+		LoginPage lp = new LoginPage(driver);
+		lp.loginToapp(URL, USERNAME, PASSWORD);
+	}
+
+	@AfterMethod(groups = {"SMOKE TEST", "REGRESSION TEST"})
+	public void configAM() {
+		System.out.println("=logout=");
+		HomePage hp = new HomePage(driver);
+		hp.logout();
+	}
+
+	@AfterClass(groups = {"SMOKE TEST", "REGRESSION TEST"})
+	public void configAC() {
+		System.out.println("==Close the BROWSER==");
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+
+	@AfterSuite(groups = {"SMOKE TEST", "REGRESSION TEST"})
+	public void configAS() throws SQLException {
+		System.out.println("===close Db , Report backUP====");
+		dbLib.closeDbconnection();
+	}
+}
