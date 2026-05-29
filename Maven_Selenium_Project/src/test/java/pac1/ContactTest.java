c45b005bc40c45ec2e2c7be7cc36c06c80ec818b
shastrideva44-bit <shastrideva44@gmail.com>
2026-05-29 15:09:31 +0530

    two delete

--- Maven_Selenium_Project/src/test/java/pac1/ContactTest.java
+++ Maven_Selenium_Project/src/test/java/pac1/ContactTest.java
@@ -6,24 +6,32 @@ public class ContactTest {
 	@Test
 	public void createContactTest() {
 		String URL = System.getProperty("url");
-		String BROWSER = System.getProperty("browser","chrome");
+		String BROWSER = System.getProperty("browser", "chrome");
 		String USERNAME = System.getProperty("username");
 		String PASSWORD = System.getProperty("password");
-		
+
 		System.out.println(URL);
 		System.out.println(BROWSER);
 		System.out.println(USERNAME);
 		System.out.println(PASSWORD);
-		
+
 		System.out.println("execute createContactTest ");
 	}
+
 	@Test
 	public void modifyContactTest() {
 		System.out.println("execute modifyContactTest ");
 	}
+
 	@Test
 	public void deleteContactTest() {
 		System.out.println("execute deleteContactTest ");
 	}
+
+	@Test
+	public void deleteTwoContactTest() {
+		System.out.println("execute deleteTwoContactTest ");
+	}
 }
+
 // check
